44066d55af849670e38ec16a034641ae0f44bae6
Li Zhang <lizhangsysu@gmail.com>
2021-03-17 21:38:28 -0700

    implement voted and average Perceptron

--- Perceptron/run_bank_note.py
+++ Perceptron/run_bank_note.py
@@ -19,4 +19,11 @@ test_file.close()
 Data_train = np.array(Data_train, dtype='float64')
 Data_test = np.array(Data_test, dtype='float64')
 
-print(Perceptron.Perceptron(Data_train, 10, 1.0))
+w1 = Perceptron.Perceptron(Data_train, 10, 1.0)
+w2 = Perceptron.Perceptron_average(Data_train, 10, 1.0)
+w3, c3 = Perceptron.Perceptron_voted(Data_train, 10, 1.0)
+
+print(Perceptron.Perceptron_test(Data_test, w1))
+print(Perceptron.Perceptron_test(Data_test, w2))
+print(Perceptron.Perceptron_voted_test(Data_test, w3, c3))
+
